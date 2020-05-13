47bd5fa7b5b82ac2e968f37935d98951a1bb993d
Freddie Lumor <freddielumor@gmail.com>
2020-05-13 23:45:10 +0100

    Add api proxy to server and client, update api paths in constants file

--- client/src/utilitites/constants.js
+++ client/src/utilitites/constants.js
@@ -1,8 +1,7 @@
-export const GET_USERS_ENDPOINT = "http://localhost:5000/users";
-export const CREATE_USER_ENDPOINT = "http://localhost:5000/users/add";
+export const GET_USERS_ENDPOINT = "/users";
+export const CREATE_USER_ENDPOINT = "/users/add";
 
-export const GET_EXERCISES_ENDPOINT = "http://localhost:5000/exercises";
-export const CREATE_EXERCISE_ENDPOINT = "http://localhost:5000/exercises/add";
-export const DELETE_EXERCISE_ENDPOINT = `http://localhost:5000/exercises`;
-export const UPDATE_EXERCISE_ENDPOINT =
-  "http://localhost:5000/exercises/update";
+export const GET_EXERCISES_ENDPOINT = "/exercises";
+export const CREATE_EXERCISE_ENDPOINT = "/exercises/add";
+export const DELETE_EXERCISE_ENDPOINT = `/exercises`;
+export const UPDATE_EXERCISE_ENDPOINT = "/exercises/update";
