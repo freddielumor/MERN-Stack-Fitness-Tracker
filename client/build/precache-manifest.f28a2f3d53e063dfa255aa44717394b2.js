7a87bfb605fc6d2d3766ca1afa06267892a7eb59
Freddie Lumor <freddielumor@gmail.com>
2020-05-15 17:22:42 +0100

    Remove build from ignore. Update path the build folder in server.js

---
+++ client/build/precache-manifest.f28a2f3d53e063dfa255aa44717394b2.js
@@ -0,0 +1,30 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "4cf3fd6c635d2aee4873de6d6e91e7c1",
+    "url": "/index.html"
+  },
+  {
+    "revision": "786e95dbfe9c32766ad0",
+    "url": "/static/css/2.9ff99f11.chunk.css"
+  },
+  {
+    "revision": "18e1ccde63283ebc1c87",
+    "url": "/static/css/main.a4caa912.chunk.css"
+  },
+  {
+    "revision": "786e95dbfe9c32766ad0",
+    "url": "/static/js/2.472a5ecd.chunk.js"
+  },
+  {
+    "revision": "5ac48c47bb3912b14c2d8de4f56d5ae8",
+    "url": "/static/js/2.472a5ecd.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "18e1ccde63283ebc1c87",
+    "url": "/static/js/main.5d1e3d10.chunk.js"
+  },
+  {
+    "revision": "0e35831b56fb2c044e12",
+    "url": "/static/js/runtime-main.98a2b520.js"
+  }
+]);
