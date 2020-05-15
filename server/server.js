d04a060ba9b4b98a8c1ce1cdd13e643af627acef
Freddie Lumor <freddielumor@gmail.com>
2020-05-15 20:03:20 +0100

    tweak

--- server/server.js
+++ server/server.js
@@ -38,7 +38,7 @@ if (process.env.NODE_ENV === "production") {
   app.use(express.static(path.join(__dirname, "../client/build/")));
 
   // Handle React routing, return all requests to React app
-  app.get("*", function (req, res) {
+  app.get("/*", (req, res) => {
     res.sendFile(path.join(__dirname + "../client/build/index.html"));
   });
 }
